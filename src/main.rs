d5f544f933cc4ebb989aaca76ebc924c1e2d7fee
Matt Hunzinger <matthunz2@gmail.com>
2023-02-04 01:53:27 -0500

    Update currency input

--- src/main.rs
+++ src/main.rs
@@ -98,13 +98,15 @@ fn app() {
                 }
                 Display::Send { address } => {
                     if let Some(address) = address {
-                        let amount = state(|| String::from(""));
+                        let amount = state(|| String::from("0"));
 
                         container(
                             Modifier::default()
                                 .align_items(AlignItems::Center)
                                 .flex_direction(FlexDirection::Column)
-                                .keyboard_handler(CurrencyInputKeyboardHandler::new(amount)),
+                                .keyboard_handler(CurrencyInputKeyboardHandler::new(
+                                    amount, currency,
+                                )),
                             move || {
                                 text(Modifier::default(), &address);
 
@@ -144,30 +146,40 @@ fn full_width_button(label: impl Into<String>, on_press: impl FnMut() + 'static)
 
 pub struct CurrencyInputKeyboardHandler {
     value: State<String>,
+    currency: State<Currency>,
 }
 
 impl CurrencyInputKeyboardHandler {
-    fn new(value: State<String>) -> Self {
-        Self { value }
+    fn new(value: State<String>, currency: State<Currency>) -> Self {
+        Self { value, currency }
     }
 
     fn push_char(&mut self, c: char) {
-        if self.value.get().as_ref().parse::<f32>().unwrap_or_default() < 1000. {
-            if let Some(pos) = self
-                .value
-                .get()
-                .cloned()
-                .chars()
-                .rev()
-                .position(|c| c == '.')
-            {
-                if pos <= 8 {
-                    self.value.get().as_mut().push(c)
-                }
-            } else {
-                self.value.get().as_mut().push(c)
+        let (max_integer, max_decimal_places) = match self.currency.get().cloned() {
+            Currency::Bitcoin => (2, 8),
+            Currency::USD => (10, 2),
+        };
+
+        if let Some(pos) = self
+            .value
+            .get()
+            .cloned()
+            .chars()
+            .rev()
+            .position(|c| c == '.')
+        {
+            if pos >= max_decimal_places {
+                return;
             }
+        } else if self.value.get().as_ref().len() > max_integer {
+            return;
         }
+
+        if &*self.value.get().as_ref() == "0" {
+            self.value.get().as_mut().pop();
+        }
+
+        self.value.get().as_mut().push(c)
     }
 }
 
@@ -179,6 +191,10 @@ impl KeyboardHandler for CurrencyInputKeyboardHandler {
                 VirtualKeyCode::Key1 | VirtualKeyCode::Numpad1 => self.push_char('1'),
                 VirtualKeyCode::Back => {
                     self.value.get().as_mut().pop();
+
+                    if self.value.get().as_ref().is_empty() {
+                        self.value.get().as_mut().push('0');
+                    }
                 }
                 VirtualKeyCode::Period => {
                     if !self.value.get().as_ref().contains('.') {
